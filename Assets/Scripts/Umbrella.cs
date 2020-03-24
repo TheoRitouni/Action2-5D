8a2a9dc4d4493d680babedc1872a69eca0fddb75
Théo <t.ritouni@gmail.com>
2020-03-24 16:45:46 +0100

    fix master

---
+++ Assets/Scripts/Umbrella.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Umbrella : MonoBehaviour
+{
+    private Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.umbrella == true)
+        {
+
+        }
+    }
+}
