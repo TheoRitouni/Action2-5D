9f5133368539eb14a6ad07f1ae74fbdab17ac56c
f.jouanneau <f.jouanneau@student.isartdigital.com>
2020-03-24 12:09:34 +0100

    Adding ui script launch menu, complete enemy script,

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -12,12 +12,13 @@ public class Enemy : MonoBehaviour
     private float errorMovementMargin = 0f;
     private bool playerInFov = false;
     private Player playerScript;
+    [SerializeField] private bool showDebug = false;
     [SerializeField] private Transform player;
 
 
     [Header("Basics info")]
     [SerializeField] [Range(0f, 10f)] private float speed = 0f;
-    [Tooltip("If color of player if higher than this or equal, enemy go on him (speed * (colorPlayer * 2))")]
+    [Tooltip("If color of player if higher than this or equal, enemy go on him (speed * colorPlayer)")]
     [SerializeField] [Range(0f, 1f)] private float seeColorPlayer = 0f;
     [Tooltip("Movement point in order, it close the path alone, dont make last point in the first")]
     [SerializeField] private Transform[] movementPoint = null;
@@ -48,7 +49,7 @@ public class Enemy : MonoBehaviour
 
         if (playerInFov && playerScript.colorPlayer >= seeColorPlayer)
         {
-            speed = naturalSpeed * (playerScript.colorPlayer * 2);
+            speed = naturalSpeed * playerScript.colorPlayer;
             navAgent.SetDestination(player.position);
         }
         else if (movementPoint.Length > 1)
@@ -83,56 +84,58 @@ public class Enemy : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.black;
-
-        for (int i = 0; i < movementPoint.Length; i++)
+        if (showDebug)
         {
-            if (i + 1 != movementPoint.Length)
-                Gizmos.DrawRay(movementPoint[i].position, movementPoint[i + 1].position - movementPoint[i].position);
-            else
-                Gizmos.DrawRay(movementPoint[i].position, movementPoint[0].position - movementPoint[i].position);
-        }
+            Gizmos.color = Color.black;
 
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, maxRadius);
+            for (int i = 0; i < movementPoint.Length; i++)
+            {
+                if (i + 1 != movementPoint.Length)
+                    Gizmos.DrawRay(movementPoint[i].position, movementPoint[i + 1].position - movementPoint[i].position);
+                else
+                    Gizmos.DrawRay(movementPoint[i].position, movementPoint[0].position - movementPoint[i].position);
+            }
 
-        Vector3 fovLine1 = Quaternion.AngleAxis(maxAngle, transform.up) * transform.forward * maxRadius;
-        Vector3 fovLine2 = Quaternion.AngleAxis(-maxAngle, transform.up) * transform.forward * maxRadius;
-        Vector3 fovLine3 = Quaternion.AngleAxis(-maxAngle + 90, transform.right) * transform.up * maxRadius;
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(transform.position, maxRadius);
 
-        Gizmos.color = Color.blue;
-        Gizmos.DrawRay(transform.position, fovLine1);
-        Gizmos.DrawRay(transform.position, fovLine2);
-        Gizmos.DrawRay(transform.position, fovLine3);
+            Vector3 fovLine1 = Quaternion.AngleAxis(maxAngle, transform.up) * transform.forward * maxRadius;
+            Vector3 fovLine2 = Quaternion.AngleAxis(-maxAngle, transform.up) * transform.forward * maxRadius;
+            Vector3 fovLine3 = Quaternion.AngleAxis(-maxAngle + 90, transform.right) * transform.up * maxRadius;
 
-        Gizmos.color = Color.magenta;
-        Gizmos.DrawRay(transform.position, transform.forward * maxRadius);
+            Gizmos.color = Color.blue;
+            Gizmos.DrawRay(transform.position, fovLine1);
+            Gizmos.DrawRay(transform.position, fovLine2);
+            Gizmos.DrawRay(transform.position, fovLine3);
+
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawRay(transform.position, transform.forward * maxRadius);
 
 
-        if (player != null)
-        {
-            if (!playerInFov)
-                Gizmos.color = Color.red;
-            else
-                Gizmos.color = Color.green;
+            if (player != null)
+            {
+                if (!playerInFov)
+                    Gizmos.color = Color.red;
+                else
+                    Gizmos.color = Color.green;
 
-            Gizmos.DrawRay(transform.position, (player.position - transform.position).normalized * maxRadius);
+                Gizmos.DrawRay(transform.position, (player.position - transform.position).normalized * maxRadius);
+            }
         }
 
     }
 
     private bool InFov()
     {
-        Collider[] overlaps = Physics.OverlapSphere(transform.position, maxRadius);
+        /*Collider[] overlaps = Physics.OverlapSphere(transform.position, maxRadius);
 
         for (int i = 0; i < overlaps.Length; i++)
         {
             if (overlaps[i] != null)
             {
                 if (overlaps[i].transform == player)
-                {
+                {*/
                     Vector3 directionBetween = (player.position - transform.position).normalized;
-                    //directionBetween.y *= 0;
 
                     float angle = Vector3.Angle(transform.forward, directionBetween);
 
@@ -148,9 +151,9 @@ public class Enemy : MonoBehaviour
                         }
                     }
 
-                }
+               /* }
             }
-        }
+        }*/
 
         return false;
     }
--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -66,14 +66,14 @@ public class Platform : MonoBehaviour
                 if (direction == false)
                 {
                     float curve = moveCurve.Evaluate(distParcouru / (initialPos.x + platformDist / 2));
-                    gameObject.transform.Translate(new Vector3(platformSpeed * curve * Time.deltaTime, 0, 0));
+                    gameObject.transform.Translate(new Vector3(platformSpeed * Time.deltaTime, 0, 0));
                     distParcouru = transform.position.x - platformDist / 2 - initialPos.x;
                 }
                 if (direction == true)
                 {
                     Debug.Log(distParcouru / (initialPos.x - platformDist / 2));
                     float curve = moveCurve.Evaluate(distParcouru / (initialPos.x - platformDist / 2));
-                    gameObject.transform.Translate(new Vector3(-platformSpeed * curve * Time.deltaTime, 0, 0));
+                    gameObject.transform.Translate(new Vector3(-platformSpeed * Time.deltaTime, 0, 0));
                     distParcouru = initialPos.x - platformDist / 2 - transform.position.x;
                 }
 
