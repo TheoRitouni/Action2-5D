4b16ef189300c8cf8e29731553027ac154f23197
f.jouanneau <f.jouanneau@student.isartdigital.com>
2020-03-24 16:49:06 +0100

    Lever script work

---
+++ Assets/Scripts/Puzzle/Lever.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    [SerializeField] private bool showDebug = false;
+
+    [SerializeField] private Color onColor = Color.green;
+    [SerializeField] private Color offColor = Color.red;
+
+    [SerializeField] private List<Platform> platforms = new List<Platform>();
+    [SerializeField] private List<OneTimeTrigger> oneTimeTriggers = new List<OneTimeTrigger>();
+    [SerializeField] private List<Enemy> ennemies = new List<Enemy>();
+
+    private bool isOn = false;
+
+    private void OnDrawGizmos()
+    {
+        if (showDebug)
+        {
+            for (int i = 0; i < platforms.Count; i++)
+            {
+                if (platforms[i].isActive)
+                {
+                    Gizmos.color = onColor;
+                    Gizmos.DrawWireSphere(platforms[i].transform.position, 1f);
+                }
+                else
+                {
+                    Gizmos.color = offColor;
+                    Gizmos.DrawWireSphere(platforms[i].transform.position, 1f);
+                }
+            }
+
+            for (int i = 0; i < oneTimeTriggers.Count; i++)
+            {
+                if (oneTimeTriggers[i].isActive)
+                {
+                    Gizmos.color = onColor;
+                    Gizmos.DrawWireSphere(oneTimeTriggers[i].transform.position, 1f);
+                }
+                else
+                {
+                    Gizmos.color = offColor;
+                    Gizmos.DrawWireSphere(oneTimeTriggers[i].transform.position, 1f);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetButtonDown("CircleButton") && other.CompareTag("Player"))
+        {
+            ChangeState();
+        }
+    }
+
+    private void ChangeState()
+    {
+        isOn = !isOn;
+
+        for (int i = 0; i < platforms.Count; i++)
+        {
+            platforms[i].isActive = !platforms[i].isActive;
+        }
+
+        for (int i = 0; i < oneTimeTriggers.Count; i++)
+        {
+            oneTimeTriggers[i].isActive = !oneTimeTriggers[i].isActive;
+        }
+
+        for (int i = 0; i < ennemies.Count; i++)
+        {
+            ennemies[i].pathA = !ennemies[i].pathA;
+        }
+    }
+}
---
+++ Assets/Scripts/Puzzle/OneTimeTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneTimeTrigger : MonoBehaviour
+{
+    public bool isActive = false;
+    private bool max = false;
+
+    [SerializeField] private bool dirX = false;
+    [SerializeField] private bool dirY = false;
+
+    [SerializeField] private float move = 1f;
+    [SerializeField] private float time = 1f;
+    private float theTime = 0f;
+
+    private void Start()
+    {
+        theTime = time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActive && !max)
+        {
+
+        }
+        else if (isActive && max)
+        {
+
+        }
+    }
+}
