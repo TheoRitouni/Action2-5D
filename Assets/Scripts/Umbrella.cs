30849d4210ce5b0b1c0020502dcaa679c45c5511
Théo <t.ritouni@gmail.com>
2020-03-24 17:55:38 +0100

    add umbrella features need input joystick

--- Assets/Scripts/Umbrella.cs
+++ Assets/Scripts/Umbrella.cs
@@ -4,19 +4,63 @@ using UnityEngine;
 
 public class Umbrella : MonoBehaviour
 {
+    [SerializeField] private float limitOfUmbrella = 0.8f;
+    [SerializeField] private float speedOfUmbrella = 0.01f;
     private Player player;
+    private Rigidbody rig;
+
+    private float distX = 0f;
+    private float distZ = 0f;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        rig = player.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(player.umbrella == true)
-        {
+        
+        rig.velocity = rig.velocity / 2f;
 
+        if (Input.GetKey(KeyCode.M))
+        {
+            if (distX < limitOfUmbrella)
+                distX += speedOfUmbrella;
         }
+        if (Input.GetKey(KeyCode.K))
+        {
+            if (distX > -limitOfUmbrella)
+                distX -= speedOfUmbrella;
+        }
+        else
+        { 
+            if( distX < 0)
+                distX += speedOfUmbrella;
+            if (distX > 0)
+                distX -= speedOfUmbrella;
+        }
+
+        if (Input.GetKey(KeyCode.O))
+        {
+            if (distZ < limitOfUmbrella)
+                distZ += speedOfUmbrella;
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+            if (distZ > -limitOfUmbrella)
+                distZ -= speedOfUmbrella;
+        }
+        else
+        {
+            if (distZ < 0)
+                distZ += speedOfUmbrella;
+            if (distZ > 0)
+                distZ -= speedOfUmbrella;
+        }
+
+
+        gameObject.transform.position = new Vector3(player.transform.position.x + distX, player.transform.position.y + 0.9f, player.transform.position.z + distZ);
     }
 }
