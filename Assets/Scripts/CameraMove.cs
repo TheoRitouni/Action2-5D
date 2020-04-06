607bd99062089c850a99d406dee0b0f20bea3735
Théo <t.ritouni@gmail.com>
2020-04-06 16:43:21 +0200

    fix jump/squat-> (fixedUpdate not adapted to all) and some bug on platform

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -53,8 +53,6 @@ public class CameraMove : MonoBehaviour
         PosPLayerDelay = player.transform.position;
         initialDistCamY = distCamY;
         initialDistCamZ = distCamZ;
-
-        //initSpeedLatency = speedLatency;
     }
 
     void Update()
@@ -64,123 +62,129 @@ public class CameraMove : MonoBehaviour
             camstart = false;
             transform.position = new Vector3(player.transform.position.x, player.transform.position.y + distCamY, player.transform.position.z - distCamZ);
         }
-        // curve for smoothness //
 
-        //if (PosPLayerDelay.magnitude < player.transform.position.magnitude + 0.01 && PosPLayerDelay.magnitude > player.transform.position.magnitude - 0.01)
-        //{
-        //    moveDur = 0;
-        //}
-        //moveDur += Time.deltaTime;
-        //speedLatency = curveLatency.Evaluate(moveDur/ TimeLatency) * initSpeedLatency;
-
-        // ========= // 
         if (!levelManager.dead && !levelManager.pause)
-        { 
-            if(player.RoofOnPlayer)
-            {
-                if(distCamY > initialDistCamY / powerZoom)
-                {
-                    distCamY -= speedZoom  / distCamZ * Time.deltaTime;
-                }
-                if (distCamZ > initialDistCamZ / powerZoom)
-                {
-                    distCamZ -= speedZoom  / distCamY * Time.deltaTime;
-                }
-            }
-            else
-            {
-                if (distCamY < initialDistCamY)
-                {
-                    distCamY += speedZoom / distCamZ * Time.deltaTime;
-                }
-                if (distCamZ < initialDistCamZ)
-                {
-                    distCamZ += speedZoom / distCamY * Time.deltaTime;
-                }
-            }
+        {
+            ZoomValue();
+            LatencyValue();
+            CameraFrontValue();
 
-            if (latency == true)
-            {
-                smoothness -= Time.deltaTime;
-                if (smoothness < 0)
-                {
-                    smoothness = initialTimeFollow;
-                    latencyDir = player.transform.position - PosPLayerDelay;
-                }
-            }
+            ManageCamera();
 
-            if (cameraFront == true)
-            {
-                float Horizontal = Input.GetAxis("Horizontal") * speedCam;
-
-                if (Horizontal > 0 && dirRight == false)
-                {
-                    dirRight = true;
-                }
-                if (Horizontal < 0 && dirRight == true)
-                {
-                    dirRight = false;
-                }
-
-                if (dirRight == true)
-                {
-                    if (posX < limitOfCam)
-                    {
-                        posX += Horizontal * Time.deltaTime;
-                        if (Horizontal == 0)
-                        {
-                            posX += speedCam / 2 * Time.deltaTime;
-                        }
-                    }
-                    if (posX > limitOfCam)
-                    {
-                        posX = limitOfCam;
-                    }
-                }
-                if (dirRight == false)
-                {
-                    if (posX > -limitOfCam)
-                    {
-                        posX += Horizontal * Time.deltaTime;
-                        if (Horizontal == 0)
-                        {
-                            posX -= speedCam / 2 * Time.deltaTime;
-                        }
-                    }
-                    if (posX < -limitOfCam)
-                    {
-                        posX = -limitOfCam;
-                    }
-                }
-            }
-
-            if (latency == true && cameraFront == false)
-            {
-                PosPLayerDelay += latencyDir * Time.deltaTime * speedLatency ;
-                transform.position = new Vector3(PosPLayerDelay.x , PosPLayerDelay.y + distCamY, PosPLayerDelay.z - distCamZ);
-                transform.LookAt(PosPLayerDelay);
-            }
-            else if(cameraFront == true && latency == false)
-            {
-                posCam.x = player.transform.position.x + posX;
-                posCam.y = player.transform.position.y;
-                posCam.z = player.transform.position.z;
-                transform.position = new Vector3(posCam.x, player.transform.position.y + distCamY, player.transform.position.z - distCamZ);
-                transform.LookAt(posCam);
-            }
-            else if (latency == true && cameraFront == true)
-            {
-                PosPLayerDelay += latencyDir * Time.deltaTime * speedLatency;
-                posCam.x = PosPLayerDelay.x + posX;
-                posCam.y = PosPLayerDelay.y;
-                posCam.z = PosPLayerDelay.z;
-                transform.position = new Vector3(PosPLayerDelay.x + posX, PosPLayerDelay.y + distCamY, PosPLayerDelay.z - distCamZ);
-                transform.LookAt(posCam);
-            }
-
-            
         }
+    }
 
+    private void ZoomValue()
+    {
+        if (player.RoofOnPlayer)
+        {
+            if (distCamY > initialDistCamY / powerZoom)
+            {
+                distCamY -= speedZoom / distCamZ * Time.deltaTime;
+            }
+            if (distCamZ > initialDistCamZ / powerZoom)
+            {
+                distCamZ -= speedZoom / distCamY * Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (distCamY < initialDistCamY)
+            {
+                distCamY += speedZoom / distCamZ * Time.deltaTime;
+            }
+            if (distCamZ < initialDistCamZ)
+            {
+                distCamZ += speedZoom / distCamY * Time.deltaTime;
+            }
+        }
+    }
 
+    private void LatencyValue()
+    {
+        if (latency == true)
+        {
+            smoothness -= Time.deltaTime;
+            if (smoothness < 0)
+            {
+                smoothness = initialTimeFollow;
+                latencyDir = player.transform.position - PosPLayerDelay;
+            }
+        }
+    }
+
+    private void CameraFrontValue()
+    {
+        if (cameraFront == true)
+        {
+            float Horizontal = Input.GetAxis("Horizontal") * speedCam;
+
+            if (Horizontal > 0 && dirRight == false)
+            {
+                dirRight = true;
+            }
+            if (Horizontal < 0 && dirRight == true)
+            {
+                dirRight = false;
+            }
+
+            if (dirRight == true)
+            {
+                if (posX < limitOfCam)
+                {
+                    posX += Horizontal * Time.deltaTime;
+                    if (Horizontal == 0)
+                    {
+                        posX += speedCam / 2 * Time.deltaTime;
+                    }
+                }
+                if (posX > limitOfCam)
+                {
+                    posX = limitOfCam;
+                }
+            }
+            if (dirRight == false)
+            {
+                if (posX > -limitOfCam)
+                {
+                    posX += Horizontal * Time.deltaTime;
+                    if (Horizontal == 0)
+                    {
+                        posX -= speedCam / 2 * Time.deltaTime;
+                    }
+                }
+                if (posX < -limitOfCam)
+                {
+                    posX = -limitOfCam;
+                }
+            }
+        }
+    }
+
+    private void ManageCamera()
+    {
+        if (latency == true && cameraFront == false)
+        {
+            PosPLayerDelay += latencyDir * Time.deltaTime * speedLatency;
+            transform.position = new Vector3(PosPLayerDelay.x, PosPLayerDelay.y + distCamY, PosPLayerDelay.z - distCamZ);
+            transform.LookAt(PosPLayerDelay);
+        }
+        else if (cameraFront == true && latency == false)
+        {
+            posCam.x = player.transform.position.x + posX;
+            posCam.y = player.transform.position.y;
+            posCam.z = player.transform.position.z;
+            transform.position = new Vector3(posCam.x, player.transform.position.y + distCamY, player.transform.position.z - distCamZ);
+            transform.LookAt(posCam);
+        }
+        else if (latency == true && cameraFront == true)
+        {
+            PosPLayerDelay += latencyDir * Time.deltaTime * speedLatency;
+            posCam.x = PosPLayerDelay.x + posX;
+            posCam.y = PosPLayerDelay.y;
+            posCam.z = PosPLayerDelay.z;
+            transform.position = new Vector3(PosPLayerDelay.x + posX, PosPLayerDelay.y + distCamY, PosPLayerDelay.z - distCamZ);
+            transform.LookAt(posCam);
+        }
     }
 }
