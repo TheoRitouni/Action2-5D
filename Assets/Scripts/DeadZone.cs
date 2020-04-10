70f651f0416478f697911e280bc42158eb5d3a06
Théo <t.ritouni@gmail.com>
2020-04-10 10:16:14 +0200

    add a dead zone on level if you fall

---
+++ Assets/Scripts/DeadZone.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour
+{
+
+    private LevelManager levelManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelManager = FindObjectOfType<LevelManager>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            levelManager.dead = true;
+        }
+    }
+}
